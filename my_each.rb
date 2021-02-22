46a56891f4c8d7af446910c8338e0c901e656e9f
Becky Rosa <becky_m_rosa@yahoo.com>
2021-02-22 00:33:32 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,12 @@
 def my_each(array)
   a = []
   while a < array.size
-    yield([a])
+    yield(array[a])
     a += 1
 end
   array
 end
 
-my_each(["1, 2, 3, 4"]) do |i|
-  "#{i} is my favorite number!"
-end
+#my_each("1, 2, 3, 4") do |i|
+#  "#{i} is my favorite number!"
+#end
